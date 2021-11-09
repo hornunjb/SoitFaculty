32291114d70931988f62d7ba2e2860b2014c2ed6
Jacob Hornung <hornunjb@mail.uc.edu>
2021-11-09 15:18:58 -0500

    register faculty form and save

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.soit.soitfaculty.entity.Faculty;
@@ -29,7 +31,28 @@ public class FacultyController {
 		// add faculties to the spring model
 		theModel.addAttribute("faculties", theFaculties);
 		
-		return "list-faculties";
+		return "faculties/list-faculties";
+	}
+	
+	@GetMapping("/viewAddForm")
+	public String viewAddForm(Model theModel) {
+		
+		// model attribute for data binding
+		Faculty theFaculty = new Faculty();
+		
+		theModel.addAttribute("faculty", theFaculty);
+		
+		return "faculties/faculty-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
+		
+		// register the faculty
+		facultyService.save(theFaculty);
+	
+		// block duplicate submission for accidental page refreshed
+		return "redirect:/Faculties/list";
 	}
 	
 
